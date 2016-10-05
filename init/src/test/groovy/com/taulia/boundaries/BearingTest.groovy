db0e24bbe191f1cce3d5f4dd0b862809d9d01ab3
brent.fisher <brent.fisher@taulia.com>
2016-10-05 16:38:40 -0600

    Bearing Test Addition

---
+++ init/src/test/groovy/com/taulia/boundaries/BearingTest.groovy
@@ -0,0 +1,47 @@
+package com.taulia.boundaries
+
+import org.junit.Test
+
+
+class BearingTest {
+  @Test(expected = BearingOutOfRangeException.class)
+  public void throwsOnNegativeNumber() {
+    new Bearing(-1)
+  }
+
+  @Test(expected = BearingOutOfRangeException.class)
+  public void throwsWhenBearingTooLarge() {
+    new Bearing(Bearing.MAX + 1)
+  }
+
+  @Test
+  public void answersValidBearing() {
+    assert new Bearing(Bearing.MAX).value() == Bearing.MAX
+  }
+
+  @Test
+  public void answersAngleBetweenItAndAnotherBearing() {
+    assert new Bearing(15).angleBetween(new Bearing(12)) == 3
+  }
+
+  @Test
+  public void angleBetweenIsNegativeWhenThisBearingSmaller() {
+    assert new Bearing(12).angleBetween(new Bearing(15)) == -3
+  }
+
+  class BearingOutOfRangeException extends Exception {
+    BearingOutOfRangeException() {
+      super()
+    }
+  }
+
+  class Bearing {
+    static int MAX
+
+    Bearing(int num) {
+
+    }
+
+    def angleBetween(Bearing bearing) {}
+  }
+}
